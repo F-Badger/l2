40625fd15b2aa7e26ec935392b0af8ef94874b3e
F-Badger <francisbajwa14@gmail.com>
2025-11-11 09:46:06 +0000

    makefiles

---
+++ COMP2221 Programming Paradigms/Lectures/Lecture 4/example_files/helper.c
@@ -0,0 +1,3 @@
+int helper(int a, int b){
+    return a+b;
+}
---
+++ COMP2221 Programming Paradigms/Lectures/Lecture 4/example_files/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "helper.h"
+
+int main(){
+    printf("Sum of %d and %d is %d\n", 1,2,helper(1,2));
+    return 0;
+}
